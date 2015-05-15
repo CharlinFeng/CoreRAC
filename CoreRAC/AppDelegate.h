56c8d1f6c3601da489744b547cfc357137918c4d
Charlin_冯成林 <1761904945@qq.com>
2015-05-15 09:06:31 +0800

    CoreRAC

---
+++ CoreRAC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreRAC
+//
+//  Created by 冯成林 on 15/5/12.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreRAC/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  CoreRAC
+//
+//  Created by 冯成林 on 15/5/12.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ReactiveCocoa.h"
+
+
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *btn;
+
+/** 登陆命令 */
+@property (nonatomic,strong) RACCommand *loginCommand;
+
+@end
+
+@implementation ViewController
+
+
+-(void)viewDidLoad{
+    
+    [super viewDidLoad];
+
+    
+}
+
+
+
+
+
+
+@end
